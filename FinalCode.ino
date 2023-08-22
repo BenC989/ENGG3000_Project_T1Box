519a7f03c0f48d7d9dee05fa027a721f69b6cf0f
BenC989 <79910849+BenC989@users.noreply.github.com>
2023-08-22 11:56:14 +1000

    Created main code file

---
+++ FinalCode.ino
@@ -0,0 +1,35 @@
+// Import Libraries
+#include <Servo.h>
+#include <Adafruit_Neopixel.h>
+
+// Initialise Variables
+
+// Setup Function
+void setup(){
+}
+
+// Loop Function
+void loop(){
+
+    // Run all the subsystems
+    detectRemote();
+    runFerrisWheel();
+    runStaircase();
+    runTrackSplitter();
+}
+
+// Detect Remote Function (BEN, QUOC)
+void detectRemote(){
+}
+
+// Run Ferris Wheel Function (IRVIN, RUMETH)
+void runFerrisWheel(){
+}
+
+// Run Staircase Function (VIKIL, THOMAS)
+void runStaircase(){
+}
+
+// Run Track Splitter Function (EUGENE)
+void runTrackSplitter(){
+}
