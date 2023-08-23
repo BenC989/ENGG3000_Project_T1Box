9022ee4d923e9af92630461df338323dea977aa9
Irvin L Hansen <80870395+Th1f@users.noreply.github.com>
2023-08-23 20:12:00 +1000

    Update FinalCode.ino
    Implemented LED code for both stairs and Ferris wheel

--- FinalCode.ino
+++ FinalCode.ino
@@ -1,11 +1,35 @@
 // Import Libraries
-#include <Servo.h>
-#include <Adafruit_Neopixel.h>
+#include <Servo.h>;
+#include <Adafruit_NeoPixel.h>;
+
 
 // Initialise Variables
 
+    // Pins Initialisation
+        
+        #define ferrisWheelPIN *placeholder*;
+        #define stairsPIN *placeholder*;
+    
+    // LED Strips Initialisation
+        
+        // Creating LED objects with the following parrameters
+        // Parameter 1: The amount of pixels in each light (Takes in integers)
+        // Parameter 2: The location of used pin for the LED strips (Takes in integers)
+        // Parameter 3: The flag colors (Takes in variables from the //Adafruit_NeoPixel.h\\ library)
+        
+        Adafruit_NeoPixel ferrisWheelLED = Adafruit_NeoPixel(120,ferrisWheelPIN,NEO_GRB);
+        Adafruit_NeoPixel stairsLED = Adafruit_NeoPixel(120,stairsPIN,NEO_GRB);
+
+    // LED Flags Initialisation
+        
+        bool ferrisWheelMotorRunning = false; //Flag if the ferris wheel motor is running
+        bool stairsMotorRunning = false; //Flag if the stairs motor is running
+
 // Setup Function
 void setup(){
+    
+    // All LED initialisation
+    setupLED();
 }
 
 // Loop Function
@@ -40,7 +64,11 @@ void runTrackSplitter(){
 
 // Ferris Wheel Lights Function (IRVIN)
 void ferrisWheelLights(){
-
+    if(ferrisWheelMotorRunning){ // If ferris wheel motor is functional turn the ferris wheel LED to green
+        colorWipe(ferrisWheelLED.Color(0,255,0) //Green
+    }else if(!ferrisWheelMotorRunning){ // If ferris wheel motor is not functional turn the ferris wheel LED to red
+        colorWipe(ferrisWheelLED.Color(255,0,0) //Red
+    }
 }
 
 // Ferris Wheel Motor Function (RUMETH)
@@ -50,10 +78,22 @@ void ferrisWheelMotor(){
 
 // Staircase Lights Function (VIKIL)
 void staircaseLights(){
-
+    if(stairsMotorRunning){ // If stairs motor is functional turn the stairs LED to green
+        colorWipe(stairsLED.Color(0,255,0) //Green
+    }else if(!stairsMotorRunning){ // If stairs motor is not functional turn the stairs LED to red
+        colorWipe(stairsLED.Color(255,0,0) //Red
+    }
 }
 
 // Staircase Motor Function (THOMAS)
 void staircaseMotor(){
 
+}
+
+// All LED setups (IRVIN)
+void setupLED(){
+    ferrisWheelLED.begin();
+    ferrisWheelLED.show();
+    stairsLED.begin();
+    stairsLED.show();
 }
