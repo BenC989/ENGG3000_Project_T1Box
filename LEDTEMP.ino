72ac5ad0bfcd354a506d9891427dd4562c6bbe42
Irvin L Hansen <80870395+Th1f@users.noreply.github.com>
2023-09-12 20:25:31 +1000

    Update LEDTEMP.ino
    Add comments to enhance clarity

--- LEDTEMP.ino
+++ LEDTEMP.ino
@@ -5,18 +5,23 @@
 #define LED_PIN  6
 #define NUM_LEDS 10
 
-int high = 0;
 
-unsigned long time = 0;
-int state = 0;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB);
+int high = 0; //This variable is not used
+
+unsigned long time = 0; //Time used to create a event-driven code
+
+int state = 0; //States used in the traffic light function
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB); 
 
 //Used to manage the states accross the different motors
+
+//Motor class to instantiate the motors and set multiple attributes that are embeded in the class
 class Motor {
   private:
-    int motorNumber;
-    int speed;
-    String status;
+    int motorNumber; 
+    int speed; 
+    String status; // Status of the current motor (e.g. STOPPED, MOVING)
     AF_DCMotor motor;
 
   public:
@@ -26,37 +31,48 @@ class Motor {
       stop();
     }
 
-    void stop() {
+    //Stops motor 
+    void stop() { 
       motor.run(RELEASE);
       status = "STOPPED";
     }
 
-    String getStatus() {
+    //Retreive and prints the motor's status
+    String getStatus() { 
+      Serial.println(status);
       return status;
     }
-    
-    void setSpeed(int speed) {
+
+    //Set motor speed
+    void setSpeed(int speed) { 
       motor.setSpeed(speed);
     }
 
-    void forward() {
+    //Run the motor in a forward motion manner
+    void forward() { 
         motor.run(FORWARD);
         status = "MOVING";
     }
 
-    void backward() {
+    //Run the motor in a backward motion manner
+    void backward() { 
         motor.run(BACKWARD);
         status = "MOVING";
     }
 };
 
+//Instantiate Motors
 Motor m1 (1);
 Motor m2 (4);
 
 void setup() {
+
+  //Configure Motors
   m1.setSpeed(100);
   m2.setSpeed(100);
-  Serial.begin(9600);
+
+  //Opens serial port and sets data rate to 9600 bits per second
+  Serial.begin(9600); 
 
   //Configure LED strip
   strip.begin();
@@ -64,19 +80,22 @@ void setup() {
 }
 
 void loop() {
-  time = millis() / 1000;
+
+  //Mutate the time to be seconds instead of milliseconds
+  time = millis() / 1000; 
   
+  //Run the motors in a forward manner
   m1.forward();
   m2.forward();
+  
   //Example of time based activity
-  if(time % 2)
+  if(time % 2) //Every 2 seconds checks for state
     if(state == 1) {
       state = 0;
     }
     else {
       state = 1;
     }
-
   delay(1000);
   if(time % 2000) {
     Serial.println(analogRead(0));
