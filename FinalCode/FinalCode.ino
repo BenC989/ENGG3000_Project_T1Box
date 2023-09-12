0f25460a3a88c44d728afd5a466e84a174a47099
BenC989 <79910849+BenC989@users.noreply.github.com>
2023-09-13 08:31:53 +1000

    clarified the main file being used

--- FinalCode/FinalCode.ino
+++ FinalCode/FinalCode.ino
@@ -1,109 +1,125 @@
-// Import Libraries
-#include <Servo.h>;
 #include <Adafruit_NeoPixel.h>;
-#include <ArduinoBLE.h>;
+#include <AFMotor.h>
+#include <Servo.h>
 
-// Initialise Variables
+#define LED_PIN  6
+#define NUM_LEDS 10
 
-// Pins Initialisation
-#define ferrisWheelPIN *placeholder*;
-#define stairsPIN *placeholder*;
 
-// LED Strips Initialisation
-    
-// Creating LED objects with the following parrameters
-// Parameter 1: The amount of pixels in each light (Takes in integers)
-// Parameter 2: The location of used pin for the LED strips (Takes in integers)
-// Parameter 3: The flag colors (Takes in variables from the //Adafruit_NeoPixel.h\\ library)
+int high = 0; //This variable is not used
 
-Adafruit_NeoPixel ferrisWheelLED = Adafruit_NeoPixel(120,ferrisWheelPIN,NEO_GRB);
-Adafruit_NeoPixel stairsLED = Adafruit_NeoPixel(120,stairsPIN,NEO_GRB);
+unsigned long time = 0; //Time used to create a event-driven code
 
-// LED Flags Initialisation
-bool ferrisWheelMotorRunning = false; //Flag if the ferris wheel motor is running
-bool stairsMotorRunning = false; //Flag if the stairs motor is running
+int state = 0; //States used in the traffic light function
 
-// Setup Function
-void setup(){
-    
-    // All LED initialisation
-    setupLED();
-}
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB); 
 
-// Loop Function
-void loop(){
+//Used to manage the states accross the different motors
 
-    // Run all the subsystems
-    detectRemote();
-    runFerrisWheel();
-    runStaircase();
-    runTrackSplitter();
-}
+//Motor class to instantiate the motors and set multiple attributes that are embeded in the class
+class Motor {
+  private:
+    int motorNumber; 
+    int speed; 
+    String status; // Status of the current motor (e.g. STOPPED, MOVING)
+    AF_DCMotor motor;
 
-// Detect Remote Function (BEN, QUOC)
-void detectRemote(){
-   
-}
-
-// Run Ferris Wheel 
-void runFerrisWheel(){
-    ferrisWheelLights();
-    ferrisWheelMotor();
-}
-
-// Run Staircase Function
-void runStaircase(){
-    staircaseLights();
-    staircaseMotor();
-}
-
-// Run Track Splitter Function (EUGENE)
-void runTrackSplitter(){
-}
-
-// Ferris Wheel Lights Function (IRVIN)
-void ferrisWheelLights(){
-    if(ferrisWheelMotorRunning){ // If ferris wheel motor is functional turn the ferris wheel LED to green
-        ferrisWheelLED.fill((0,255,0)); //Green
-    }else if(!ferrisWheelMotorRunning){ // If ferris wheel motor is not functional turn the ferris wheel LED to red
-        ferrisWheelLED.fill((255,0,0)); //Red
+  public:
+    Motor(int motorNumber) : motorNumber(motorNumber), motor(motorNumber)
+    {
+      speed = 0;
+      stop();
     }
-}
 
-// Ferris Wheel Motor Function (RUMETH)
-void ferrisWheelMotor(){
-    // Pseudocode (IRVIN)
-    
-        // runMotor(); *the function will include ferrisWheelMotorRunning = true*
-        // if(motorSpeed <= *a small number close to zero to create buffer for errors*)
-            // ferrisWheelMotorRunning = false;
-            // killSwitch(); *turns off everything (e.g. motors, splitter)* 
-
-}
-
-// Staircase Lights Function (VIKIL)
-void staircaseLights(){
-    if(stairsMotorRunning){ // If stairs motor is functional turn the stairs LED to green
-        stairsLED.fill((0,255,0)); //Green
-    }else if(!stairsMotorRunning){ // If stairs motor is not functional turn the stairs LED to red
-        stairsLED.fill((255,0,0)); //Red
+    //Stops motor 
+    void stop() { 
+      motor.run(RELEASE);
+      status = "STOPPED";
     }
+
+    //Retreive and prints the motor's status
+    String getStatus() { 
+      Serial.println(status);
+      return status;
+    }
+
+    //Set motor speed
+    void setSpeed(int speed) { 
+      motor.setSpeed(speed);
+    }
+
+    //Run the motor in a forward motion manner
+    void forward() { 
+        motor.run(FORWARD);
+        status = "MOVING";
+    }
+
+    //Run the motor in a backward motion manner
+    void backward() { 
+        motor.run(BACKWARD);
+        status = "MOVING";
+    }
+};
+
+//Instantiate Motors
+Motor m1 (1);
+Motor m2 (4);
+
+void setup() {
+
+  //Configure Motors
+  m1.setSpeed(100);
+  m2.setSpeed(100);
+
+  //Opens serial port and sets data rate to 9600 bits per second
+  Serial.begin(9600); 
+
+  //Configure LED strip
+  strip.begin();
+  strip.setBrightness(125);
 }
 
-// Staircase Motor Function (THOMAS)
-void staircaseMotor(){
-    // Pseudocode (IRVIN)
-    
-        // runMotor(); *runMotor() will include stairsMotorRunning = true*
-        // if(motorSpeed <= *a small number close to zero to create buffer for errors*)
-            // stairsMotorRunning = false;
-            // killSwitch(); *turns off everything (e.g. motors, splitter)*  
+void loop() {
+
+  //Mutate the time to be seconds instead of milliseconds
+  time = millis() / 1000; 
+  
+  //Run the motors in a forward manner
+  m1.forward();
+  m2.forward();
+  
+  //Example of time based activity
+  if(time % 2) //Every 2 seconds checks for state
+    if(state == 1) {
+      state = 0;
+    }
+    else {
+      state = 1;
+    }
+  delay(1000);
+  if(time % 2000) {
+    Serial.println(analogRead(0));
+    Serial.println(analogRead(2));
+  }
+
+  traffic(state);
+  //Serial.println(time);
 }
 
-// All LED setups (IRVIN)
-void setupLED(){
-    ferrisWheelLED.begin();
-    ferrisWheelLED.show();
-    stairsLED.begin();
-    stairsLED.show();
+
+void traffic(int state) {
+  //clear strip to recieve new information
+  strip.clear();
+  uint32_t colour = 0;
+  //If something is blocking the sensor the strip will be set to red, green if sensor is not blocked
+  if(state == 1) {
+    colour = strip.Color(255, 0, 0); 
+  }
+  else if(state == 0) {
+    colour = strip.Color(0, 255, 0);
+  }
+
+  strip.fill(colour, 0, NUM_LEDS - 1);
+  strip.show();
 }
+
